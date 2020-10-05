655d4548e989fa8eb0648d8c0d31c310c42cb27d
Abilash Janakiraman <abilashjram@gmail.com>
2020-10-05 18:02:32 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import random
+from flask import Flask, render_template, request, make_response, redirect, url_for
+from models import User, db
+
+
+app = Flask(__name__)
+
+#create database
+db.create_all()
+
+
+
+@app.route("/", methods=["GET"])
+def index():
+    email_address = request.cookies.get("email")
+
+    if email_address:
+        user = db.query(User).filter_by(email=email_address).first()
+    else:
+        user = None
+    return render_template("index.html", user=user)
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
+
+    #create the secret number
+    secret_number =  random.randint(1,30)
+
+    user = db.query(User).filter_by(email=email).first()
+
+    if not user:
+        user = User(name=name, email=email, secret_number=secret_number)
+
+        # save it into db
+        db.add(user)
+        db.commit()
+
+    response = make_response(redirect(url_for("index")))
+    response.set_cookie("email", email)
+
+    return response
+
+@app.route("/result", methods=["POST"])
+def result():
+    guess = int(request.form.get("guess"))
+
+    email_address = request.cookies.get("email")
+    user = db.query(User).filter_by(email=email_address).first()
+
+
+    if guess == user.secret_number :
+        message = "Congrats! The secret number is {0}".format(str(user.secret_number))
+
+        new_secret = random.randint(1,30)
+        user.secret_number = new_secret
+        # save it into db
+        db.add(user)
+        db.commit()
+
+    elif guess > user.secret_number:
+        message = "Wrong! Try something smaller"
+    else:
+        message = "Wrong! Try something bigger"
+
+    return render_template("result.html", message=message)
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,11 @@
+import os
+from sqla_wrapper import SQLAlchemy
+
+db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    secret_number = db.Column(db.Integer, unique=False)
+
